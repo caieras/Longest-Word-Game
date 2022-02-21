08946232c6de1ff242d98accc1898dd36f228c15
c <caio.asevedo@gmail.com>
2022-02-21 19:12:56 -0300

    added score view

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,27 @@
+require "open-uri"
+
+class GameController < ApplicationController
+
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'game#new'
+  get '/new', to: 'game#new'
+  post '/score', to: 'game#score'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
